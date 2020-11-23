c5dcd57e8fb9ed56508f7445c8f3393ba413d6c7
홍준식 <jshong0907@naver.com>
2020-11-23 23:45:32 +0900

    intwritable to text

--- assignment08/MatMul.java
+++ assignment08/MatMul.java
@@ -80,7 +80,7 @@ public class MatMul {
     //job.setCombinerClass(IntSumReducer.class);
     job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
